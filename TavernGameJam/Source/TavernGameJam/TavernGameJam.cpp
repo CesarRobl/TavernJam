01a3732cb0f4710ee4f14020e1f7920d41c4f4b8
CESAR\cesar <cesarrobles163@yahoo.com>
2025-05-20 14:49:23 -0400

    Deleted game rq

--- TavernGameJam/Source/TavernGameJam/TavernGameJam.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TavernGameJam.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TavernGameJam, "TavernGameJam" );
-
-DEFINE_LOG_CATEGORY(LogTavernGameJam)
- 
