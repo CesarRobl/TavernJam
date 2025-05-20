09ae3a4859b093d1f93a30d149e6200dce068904
CESAR\cesar <cesarrobles163@yahoo.com>
2025-05-20 13:19:49 -0400

    Pushed project to github

---
+++ TavernGameJam/Source/TavernGameJam/TavernGameJam.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TavernGameJam.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TavernGameJam, "TavernGameJam" );
+
+DEFINE_LOG_CATEGORY(LogTavernGameJam)
+ 
